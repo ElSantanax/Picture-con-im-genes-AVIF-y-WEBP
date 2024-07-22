0299db3ed84a28fb26ff2ac642eeefde829798f0
José Santana <75258412+ElSantanax@users.noreply.github.com>
2024-07-22 11:22:38 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
